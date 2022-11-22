4d212f66b4c3dc66b63c3b1ffbcb26937f46e570
mnguyener <michellehnguyen03@gmail.com>
2022-11-22 03:06:15 -0600

    created draft of logo with css file for the div component

---
+++ components/logo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "styles/logo.module.css";
+import { AspectRatio, Image } from '@mantine/core';
+
+
+function Logo() {
+    return (
+        <div className={styles.divide}>
+            <AspectRatio ratio={720/1084}  mx="lg">
+                <Image width={70} src= "favicon.ico" alt="red cougar logo" ></Image>
+            </AspectRatio>
+        </div>
+    );
+}
+
+export default Logo;
